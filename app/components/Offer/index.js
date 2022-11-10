84bf5a50c52bf3813a6b8383878dac629ffdbd92
Denys Zabohonskyi <2saret@gmail.com>
2022-11-10 21:37:20 +0200

    offer texts

--- app/components/Offer/index.js
+++ app/components/Offer/index.js
@@ -1,20 +1,37 @@
+import ReactHtmlParser from "react-html-parser";
 import classNames from "classnames";
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import texts from "./content.json";
 import styles from "./index.module.scss";
 
 const Offer = ({ active }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const changeOffer = (index) => {
+    setActiveTab(index);
+  };
+
+  useEffect(() => {
+    !active && setActiveTab(0);
+  }, [active]);
+
   return (
-    <section className={styles.offer}>
+    <section
+      className={classNames(styles.offer, {
+        [styles.web]: activeTab === 1,
+        [styles.mobile]: activeTab === 2,
+        [styles.consulting]: activeTab === 3,
+        [styles.services]: activeTab === 4,
+        [styles.cloud]: activeTab === 5,
+        [styles.design]: activeTab === 6,
+      })}
+    >
       <div className={styles.text}>
-        <h2>
-          What we <span>Offer</span>
-        </h2>
-        <p>
-          Integration with the cloud which will help your project minimize
-          development and operational costs. Integration with the cloud which
-          will help your project minimize development and operational costs.
-        </p>
+        <h2>{ReactHtmlParser(texts[activeTab].title)}</h2>
+        <p>{texts[activeTab].text}</p>
       </div>
+
       <div className={styles.menuBlock}>
         <div
           className={classNames(styles.menuShadow, {
@@ -27,12 +44,48 @@ const Offer = ({ active }) => {
           })}
         >
           <div className={classNames(styles.menu, { fadeIn: active })}>
-            <a href="#">Web</a>
-            <a href="#">Mobile</a>
-            <a href="#">Consulting</a>
-            <a href="#">Services</a>
-            <a href="#">Cloud</a>
-            <a href="#">Design</a>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 1 })}
+              onClick={() => changeOffer(1)}
+            >
+              Web
+            </Link>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 2 })}
+              onClick={() => changeOffer(2)}
+            >
+              Mobile
+            </Link>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 3 })}
+              onClick={() => changeOffer(3)}
+            >
+              Consulting
+            </Link>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 4 })}
+              onClick={() => changeOffer(4)}
+            >
+              Services
+            </Link>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 5 })}
+              onClick={() => changeOffer(5)}
+            >
+              Cloud
+            </Link>
+            <Link
+              href="#"
+              className={classNames({ [styles.active]: activeTab === 6 })}
+              onClick={() => changeOffer(6)}
+            >
+              Design
+            </Link>
           </div>
         </div>
       </div>
