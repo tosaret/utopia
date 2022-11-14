4b3886c743c7d00b6116ac012e719f3641694f7a
Denys Zabohonskyi <2saret@gmail.com>
2022-11-14 14:43:32 +0200

    first screen fixes

--- app/components/FirstScreen/Hero/index.js
+++ app/components/FirstScreen/Hero/index.js
@@ -1,15 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 import classNames from "classnames";
 
-import HeroImageSrc from "../../../../public/hero.png";
+import HeroImageSrc from "../../../../public/hero-mobile.png";
 import styles from "./index.module.scss";
 
 const Hero = ({ animate }) => {
   return (
     <>
       <div className={styles.hero}>
-        <div className={`fadeUp ${styles.image}`}>
-          <Image src={HeroImageSrc} width={700} alt="" priority={true} />
+        <div className={styles.image}>
+          <Image
+            className="fadeUpMobilePhone"
+            src={HeroImageSrc}
+            width={488}
+            alt=""
+            priority={true}
+          />
         </div>
         <div className={styles.text}>
           <h1>
@@ -19,6 +26,11 @@ const Hero = ({ animate }) => {
           <h2 className={classNames({ typing: animate })}>
             design your perfect <strong>world</strong>
           </h2>
+          <div className={styles.readMore}>
+            <Link href="#">
+              read <span>more</span>
+            </Link>
+          </div>
         </div>
       </div>
     </>
