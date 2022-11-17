06ac177ac3e39c153e38ea58f180f087b9f8adb2
Denys Zabohonskyi <2saret@gmail.com>
2022-11-17 17:28:31 +0200

    portfolio component added

--- app/components/PortfolioIntro/index.js
+++ app/components/PortfolioIntro/index.js
@@ -1,5 +1,18 @@
+import styles from "./index.module.scss";
+
 const PortfolioIntro = () => {
-  return <div>PortfolioIntro</div>;
+  return (
+    <div className={styles.portfolioIntro}>
+      <div>
+        <h2>Our Portfolio</h2>
+        <p>
+          <strong>Utopia</strong> is your backbone of creating your dream
+          application. Here are some of the <strong>World Class</strong>{" "}
+          products we are proud to be a part of.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioIntro;
