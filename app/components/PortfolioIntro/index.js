31d045594d130815577a16dfab93f5bb59c7547a
Denys Zabohonskyi <2saret@gmail.com>
2022-11-18 16:47:50 +0200

    fixes

--- app/components/PortfolioIntro/index.js
+++ app/components/PortfolioIntro/index.js
@@ -7,7 +7,7 @@ const PortfolioIntro = () => {
         <h2>Our Portfolio</h2>
         <p>
           <strong>Utopia</strong> is your backbone of creating your dream
-          application. Here are some of the <strong>World Class</strong>{" "}
+          application. <br /> Here are some of the <strong>World Class</strong>{" "}
           products we are proud to be a part of.
         </p>
       </div>
