22242dfe130e7ef722ef89c8c139299a3f6cba2f
Denys Zabohonskyi <2saret@gmail.com>
2022-11-19 19:05:58 +0200

    Portfolio Intro page added

--- app/components/PortfolioIntro/index.js
+++ app/components/PortfolioIntro/index.js
@@ -1,15 +1,21 @@
+import Image from "next/image";
+import PlayIcon from "../../../public/play-icon.svg";
 import styles from "./index.module.scss";
 
 const PortfolioIntro = () => {
   return (
     <section className={styles.portfolioIntro}>
-      <div>
+      <div className={styles.background} />
+      <div className={styles.intro}>
         <h2>Our Portfolio</h2>
         <p>
           <strong>Utopia</strong> is your backbone of creating your dream
           application. <br /> Here are some of the <strong>World Class</strong>{" "}
           products we are proud to be a part of.
         </p>
+        <a href="#" className={styles.playIcon}>
+          <Image src={PlayIcon} width={24} height={24} alt="" />
+        </a>
       </div>
     </section>
   );
