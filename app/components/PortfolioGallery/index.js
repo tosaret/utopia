da1651b37d9370d98cf9a327fc97b3e040bb39aa
Denys Zabohonskyi <2saret@gmail.com>
2022-11-22 15:29:12 +0200

    import portfolio items as array

--- app/components/PortfolioGallery/index.js
+++ app/components/PortfolioGallery/index.js
@@ -6,6 +6,10 @@ import items from "./content.json";
 import styles from "./index.module.scss";
 
 const PortfolioGallery = () => {
+  // var portfolioItems = [];
+  // for (var i in items) portfolioItems.push([i, items[i]]);
+  // console.log(portfolioItems);
+
   const [currentItem, setCurrentItem] = useState(1);
 
   const item = items[currentItem];
