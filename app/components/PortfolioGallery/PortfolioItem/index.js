3463bfaa1314a5a2dc3f05bd792f1a9d6c5ef2d7
Denys Zabohonskyi <2saret@gmail.com>
2022-11-22 14:54:31 +0200

    portfolio content

--- app/components/PortfolioGallery/PortfolioItem/index.js
+++ app/components/PortfolioGallery/PortfolioItem/index.js
@@ -1,38 +1,48 @@
-import Image from "next/image";
-
 import styles from "./index.module.scss";
 
-import imageUrl from "../../../../public/portfolio-img1.png";
-import logoUrl from "../../../../public/portfolio-logo1.png";
+const PortfolioItem = ({ item }) => {
+  const {
+    title,
+    description,
+    tags,
+    tagsColor,
+    backgroundGradient,
+    logoUrl,
+    imageUrl,
+  } = item;
 
-const PortfolioItem = () => {
   return (
     <div className={styles.portfolioGallery}>
       <div className={styles.portfolioContainer}>
         <div className={styles.info}>
           <div className={styles.titleContainer}>
             <div className={styles.title}>
-              <Image src={logoUrl} width={46} height={46} alt="" />
-              Sapiency
+              <img src={logoUrl} width={46} height={46} alt="" />
+              {title}
             </div>
             <div>
-              <span className={styles.label}>Android</span>
-              <span className={styles.label}>iOS</span>
+              {tags.map((tag) => (
+                <span
+                  style={{ backgroundColor: tagsColor }}
+                  className={styles.label}
+                >
+                  {tag}
+                </span>
+              ))}
             </div>
           </div>
           <div className={styles.descriptionContainer}>
-            <p className={styles.description}>
-              Some very long and irrelevant motivational description that nobody
-              will give a shit about but must be provided in order to keep the
-              layout interesting.
-            </p>
+            <p className={styles.description}>{description}</p>
           </div>
         </div>
         <div className={styles.image}>
-          <Image src={imageUrl} alt="" />
+          <img src={imageUrl} alt="" />
         </div>
       </div>
-      <div className={styles.portfolioBottomBg}></div>
+      <div
+        style={{ background: backgroundGradient }}
+        className={styles.portfolioBottomBg}
+      ></div>
     </div>
   );
 };
--- app/components/PortfolioGallery/index.js
+++ app/components/PortfolioGallery/index.js
@@ -1,9 +1,16 @@
+import { useState, useEffect } from "react";
 import PortfolioItem from "./PortfolioItem";
+
+import items from "./content.json";
 
 import styles from "./index.module.scss";
 
 const PortfolioGallery = () => {
-  return <PortfolioItem />;
+  const [currentItem, setCurrentItem] = useState(1);
+
+  const item = items[currentItem];
+
+  return <PortfolioItem item={item} />;
 };
 
 export default PortfolioGallery;
